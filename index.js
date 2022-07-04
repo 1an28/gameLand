85d340734a761055fcde700abdfd486729e1fac8
1an28 <1an28a0@gmail.com>
2022-07-05 01:01:22 +0900

    部屋に入室するまでのギミックを作成

--- index.js
+++ index.js
@@ -6,22 +6,41 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const roomList = []; 
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (Socket) => {
-    console.log('a user connected');
+    console.log('[LOG] a [' + Socket.id + '] connected');
 
-    Socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    let usedRoomNumber = -1;
+
+    Socket.on('match search', () => {
+        if (roomList.length === 0) {
+            roomList.push(0);
+        } else if (roomList[roomList.length - 1] === 2) {
+            roomList.push(0);
+        }
+
+        usedRoomNumber = roomList.length;
+        Socket.join(usedRoomNumber.toString());
+        roomList[usedRoomNumber - 1] = roomList[usedRoomNumber - 1] + 1;
+        
+        console.log(roomList);
+
+        Socket.emit('join log', usedRoomNumber);
     });
 
-    Socket.on('disconnect', () => {
-        console.log('user disconnected');
+    Socket.on('disconnect', () => {        
+        roomList[usedRoomNumber - 1] = roomList[usedRoomNumber - 1] - 1;
+        Socket.leave(usedRoomNumber.toString());
+
+        console.log('[LOG] [' + Socket.id + '] disconnected');
     });
 });
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+    console.log('[LOG] listening on *:3000');
 });
